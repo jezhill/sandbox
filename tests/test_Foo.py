f330323073e2e2939df4ba8284b83351faa8ce0c
Jez Hill <jezhill@gmail.com>
2019-01-14 14:13:22 -0500

    compare running test script directly vs pytest

--- tests/test_Foo.py
+++ tests/test_Foo.py
@@ -2,7 +2,9 @@
 import Foo
 
 def test_Baz():
-	assert Foo.Bar.Baz() == ''
-	assert Foo.Bar.Baz( 1 ) == '1'
-	assert Foo.Bar.Baz( 1, 2, 3 ) == '1-2-3'
+	assert Foo.Bar.Baz() == ' '
+	assert Foo.Bar.Baz( 1 ) == '1 '
+	assert Foo.Bar.Baz( 1, 2, 3 ) == '1-2-3 '
 	
+if __name__ == '__main__':
+	test_Baz()
